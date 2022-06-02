f04df5d1385344a5500951bf0de61f61f71df5b1
Juan Ospina <juanospina11@gmail.com>
2022-06-02 16:52:58 -0400

    Adding items feature based on whse - fix whseinfo

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,7 +1,165 @@
-import React from "react";
+import { arrayUnion, doc, setDoc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { db } from "../firebase";
 
-const AddItemForm = () => {
-  return <div>AddItemForm</div>;
+const AddItemForm = ({ currWhse }) => {
+  const [brand, setBrand] = useState("");
+  const [caseCount, setCaseCount] = useState("");
+  const [caseWeight, setCaseWeight] = useState("");
+  const [cogs, setCogs] = useState("");
+  const [itemName, setItemName] = useState("");
+  const [lotNumber, setLotNumber] = useState("");
+  const [poNumber, setPoNumber] = useState("");
+  const [received, setReceived] = useState("");
+  const [salesPrice, setSalesPrice] = useState("");
+  const [size, setSize] = useState("");
+  // CREATE RECEIVED DATE
+
+  // State that controls the current warehouse
+  console.log(currWhse);
+
+  // Push items to the DB
+  const addItemToWhse = async (newItem) => {
+    console.log(newItem);
+    const whseRef = doc(db, "warehouses", currWhse);
+    await updateDoc(whseRef, {
+      Items: arrayUnion(newItem),
+    });
+  };
+
+  const createItem = (e) => {
+    e.preventDefault();
+    const newItem = {
+      brand: brand,
+      caseCount: caseCount,
+      caseWeight: caseWeight,
+      cogs: cogs,
+      itemName: itemName,
+      lotNumber: lotNumber,
+      poNumber: poNumber,
+      // received: received,
+      salesPrice: +salesPrice,
+      size: size,
+    };
+    console.log(newItem);
+    addItemToWhse(newItem);
+  };
+
+  /////////////////////////
+  // Change number type to text
+
+  // onChange is making this component mount every time
+  return (
+    <form onSubmit={createItem} className="w-full flex flex-col py-4">
+      <label>
+        {" "}
+        Brand:
+        <input
+          onChange={(e) => setBrand(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="text"
+          placeholder="Brand"
+        />
+      </label>
+
+      <label>
+        Case Count:
+        <input
+          onChange={(e) => setCaseCount(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="number"
+          step="any"
+          placeholder="Case Count"
+        />
+      </label>
+
+      <label>
+        Case Weight:
+        <input
+          onChange={(e) => setCaseWeight(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="number"
+          step="any"
+          placeholder="Case Weight"
+        />
+      </label>
+
+      <label>
+        Cost of Goods:
+        <input
+          onChange={(e) => setCogs(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="number"
+          step="any"
+          placeholder="Cost of Goods"
+        />
+      </label>
+
+      <label>
+        Item Name:
+        <input
+          onChange={(e) => setItemName(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="text"
+          placeholder="Item Name"
+        />
+      </label>
+
+      <label>
+        Lot Number:
+        <input
+          onChange={(e) => setLotNumber(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="text"
+          placeholder="Lot Number"
+        />
+      </label>
+
+      <label>
+        PO Number:
+        <input
+          onChange={(e) => setPoNumber(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="text"
+          placeholder="PO Number"
+        />
+      </label>
+
+      <label>
+        Received:
+        <input
+          onChange={(e) => setReceived(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="Date"
+          placeholder="Date Received"
+        />
+      </label>
+
+      <label>
+        Sales Price:
+        <input
+          onChange={(e) => setSalesPrice(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="number"
+          step="any"
+          placeholder="Sales Price"
+        />
+      </label>
+
+      <label>
+        Size:
+        <input
+          onChange={(e) => setSize(e.target.value)}
+          className="p-3 my-2 bg-gray-700 rouded"
+          type="text"
+          placeholder="Size"
+        />
+      </label>
+      <button className="bg-red-600 py-3 my-6 rounded font-bold">
+        Add Item
+      </button>
+    </form>
+  );
 };
 
 export default AddItemForm;
--- src/components/warehouseComponents/WarehouseInfo.jsx
+++ src/components/warehouseComponents/WarehouseInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { db } from "../../firebase";
 import WarehouseInventory from "./WarehouseInventory";
 import {
@@ -12,7 +12,9 @@ import {
   getDoc,
 } from "firebase/firestore";
 import { FaPlusCircle } from "react-icons/fa";
-import WarehouseSelectBtn from "./WarehouseSelectBtn";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import AddItemForm from "../AddItemForm";
 
 ////////////
 // Synchrony
@@ -28,20 +30,49 @@ import WarehouseSelectBtn from "./WarehouseSelectBtn";
 // State should change based on the selection from the dropdown menu
 ///// Need to find how to return that value from WarehouseSelectBtn
 const WarehouseInfo = () => {
+  // List of warehouses in the DB
   const [warehouseCollection, setWarehouseCollection] = useState([]);
-  const [curWhse, setCurWhse] = useState({});
   const [warehouseInfo, setWarehouseInfo] = useState([]);
   const [inventory, setInventory] = useState([]);
   const [hidden, setHidden] = useState(true);
 
-  // Setting the initial data is not working, need to find how to make this run after getting the data from the above useEffect after it has filled the array
-  const initialWhseData = async () => {
-    const docSnap = await getDoc(doc(db, "warehouses", warehouseCollection[0]));
-    console.log(
-      `This is inside initialWhseData:` + JSON.stringify(docSnap.data())
-    );
+  // const chosenWhseData = async (whseName) => {
+  //   onSnapshot
+  // }
+
+  //////////////////////////////
+  // STATE RESETS TO FIRST WAREHOUSE IN THE ARRAY IF YOU SWITCH PAGES AND COME BACK
+  const initialWhseData = async (firstWhse) => {
+    const docSnap = await getDoc(doc(db, "warehouses", firstWhse));
+    // console.log(
+    //   `This is inside initialWhseData:` + JSON.stringify(docSnap.data())
+    // );
+    // setCurWhse(docSnap.data());
     setWarehouseInfo(docSnap.data()?.information);
     setInventory(docSnap.data()?.Items);
+  };
+
+  const getWhseFromDropdown = (e) => {
+    e.preventDefault();
+    // Target value is not working - could not find the value field upon inspection - using text since it is currently the same as value
+    const selectedWhse = e.target.text;
+    console.log("you selected" + selectedWhse);
+    onSnapshot(doc(db, "warehouses", selectedWhse), (doc) => {
+      setInventory(doc.data()?.Items);
+      setWarehouseInfo(doc.data()?.information);
+    });
+  };
+
+  const currentWhse = () => {
+    // Based on the selected whse name, set the info and inventory
+  };
+
+  //////////////////////////
+  // HANDLERS SECTION
+
+  const handleAddItemClick = () => {
+    // This will be used to hide the inventory and present a for to add an item to the current warehouse.
+    setHidden(!hidden);
   };
 
   // Get all warehouses available
@@ -52,52 +83,76 @@ const WarehouseInfo = () => {
       res.forEach((doc) => {
         // console.log(doc.id, "=>", doc.data());
         warehouseOptions.push(doc.data().information[0].name);
-        console.log("This is inside the forEach" + warehouseOptions);
+        // console.log("This is inside the forEach" + warehouseOptions);
       });
       setWarehouseCollection(warehouseOptions);
+      // console.log(warehouseOptions);
+      initialWhseData(warehouseOptions[0]);
     }
     getCollectionIDs();
-    // Needs to happen after the array has been filled from the above async function
   }, []);
   // console.log(warehouseCollection);
-
-  // initialWhseData();
-
-  ////////////////////////////////////
-  // Current way of handling the initial data - the below will be based on the current warehouse - need to get the inital data above
-
-  // This needs to be based on the selected warehouse from a dropdown
-  // useEffect(() => {
-  //   onSnapshot(doc(db, "warehouses", warehouseCollection[0]), (doc) => {
-  //     setInventory(doc.data()?.Items);
-  //   });
-  // }, []);
-
-  // // console.log(inventory);
-
-  // useEffect(() => {
-  //   onSnapshot(doc(db, "warehouses", warehouseCollection[0]), (doc) => {
-  //     setWarehouseInfo(doc.data()?.information);
-  //   });
-  // }, []);
-  // Since this is only one warehouse, I need to destructure it to grab the info
-
-  const [whseInfo] = warehouseInfo;
+  if (!warehouseInfo) {
+    return;
+  } else {
+    const [whseInfo] = warehouseInfo;
+  }
   // console.log(warehouseInfo);
 
-  const handleAddItemClick = () => {
-    // This will be used to hide the inventory and present a for to add an item to the current warehouse.
-    setHidden(!hidden);
-  };
+  // THIS FUNCTION IS PART OF THE PREBUILT DROPDOWN MENU
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
 
   return (
     <>
-      <div className="grid grid-cols-6 gap-4 m-2">
-        <WarehouseSelectBtn
-          whseArr={warehouseCollection}
-          key={"whseSelector"}
-        />
-      </div>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+            Warehouses
+            <ChevronDownIcon
+              className="-mr-1 ml-2 h-5 w-5"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              {warehouseCollection.map((whse) => {
+                return (
+                  <Menu.Item key={whse}>
+                    {({ active }) => (
+                      <a
+                        onClick={getWhseFromDropdown}
+                        value={whse}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        {whse}
+                      </a>
+                    )}
+                  </Menu.Item>
+                );
+              })}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+
       {/* I need to make the whse information section mobile responsive */}
       <div className="grid">
         <div className="grid grid-cols-2">
@@ -135,9 +190,9 @@ const WarehouseInfo = () => {
           </div>
         </div>
 
-        <h1 className={`bg-gray-600 ${hidden === false ? "" : "hidden"}`}>
-          TESTING HIDDEN
-        </h1>
+        <div className={`bg-gray-600 ${hidden === false ? "" : "hidden"}`}>
+          <AddItemForm currWhse={whseInfo?.name} key={"itemForm"} />
+        </div>
 
         {/* <form className={`bg-gray-600 ${}`} ></form> */}
 
@@ -157,7 +212,11 @@ const WarehouseInfo = () => {
           </thead>
           <tbody className="text-center">
             {inventory.map((item) => {
-              return <WarehouseInventory item={item} key={item?.lotNumber} />;
+              if (!item) {
+                return;
+              } else {
+                return <WarehouseInventory item={item} key={item?.lotNumber} />;
+              }
             })}
           </tbody>
         </table>

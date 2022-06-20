cfe79c50bad2315b0118c3f6414d691c208ffb57
Juan Ospina <juanospina11@gmail.com>
2022-06-20 16:21:32 -0400

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -7,22 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
-//   appId: process.env.REACT_APP_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAuLI16xyCs1Yq1eR23hhgkaUkRyzsOf1E",
-  authDomain: "aqua-dashboard-2ea38.firebaseapp.com",
-  projectId: "aqua-dashboard-2ea38",
-  storageBucket: "aqua-dashboard-2ea38.appspot.com",
-  messagingSenderId: "334476247978",
-  appId: "1:334476247978:web:e2e4d82874f44023bf21d3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase

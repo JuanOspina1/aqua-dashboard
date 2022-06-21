b1dff8472faea6b6045c2ba3917de25b0750609f
Juan Ospina <juanospina11@gmail.com>
2022-06-21 16:21:12 -0400

    began implementing login page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import WarehouseMgnt from "./pages/WarehouseMgnt";
 import Home from "./pages/Home";
 import Warehouses from "./pages/Warehouses";
 import Orders from "./pages/Orders";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <div className="flex ">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/warehouses" element={<Warehouses />} />
           <Route path="/management" element={<WarehouseMgnt />} />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import Sidebar from "../components/Sidebar";
+import bgImage from "../images/pexels-tiger-lily-4483610.jpg";
 
 const Login = () => {
-  return <></>;
+  const login = () => {};
+
+  return (
+    <div className="w-full h-screen">
+      <img
+        className="absolute w-full h-full object-cover"
+        src={bgImage}
+        alt="/"
+      />
+      <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+    </div>
+  );
 };
 
 export default Login;

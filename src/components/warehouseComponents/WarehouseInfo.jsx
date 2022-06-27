5a44f739f4b92f3d47a2b389cd6610aea9d44786
Juan Ospina <juanospina11@gmail.com>
2022-06-27 11:36:15 -0400

    started changing bg color

--- src/components/warehouseComponents/WarehouseInfo.jsx
+++ src/components/warehouseComponents/WarehouseInfo.jsx
@@ -112,7 +112,7 @@ const WarehouseInfo = () => {
     <>
       <Menu as="div" className="relative inline-block text-left ml-4 mb-2">
         <div>
-          <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+          <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-slate-200 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
             Select a Warehouse
             <ChevronDownIcon
               className="-mr-1 ml-2 h-5 w-5"
@@ -160,7 +160,7 @@ const WarehouseInfo = () => {
       {/* I need to make the whse information section mobile responsive */}
       <div className="grid ">
         <div className="grid grid-cols-2 gap-6">
-          <div className="grid grid-cols-2 ml-4 bg-white rounded-md shadow-lg">
+          <div className="grid grid-cols-2 ml-4 bg-slate-200  rounded-md shadow-lg">
             <div className="whse-info">
               <h1 className="font-bold">Warehouse Name</h1>
               <p>{whseInfo?.name}</p>
@@ -182,7 +182,7 @@ const WarehouseInfo = () => {
             </div>
           </div>
 
-          <div className="grid grid-cols-2 justify-items-center bg-white rounded-md shadow-lg mr-4">
+          <div className="grid grid-cols-2 justify-items-center bg-slate-200  rounded-md shadow-lg mr-4">
             {/* When I toggle Hidden, change the title to say: "Return to Inventory" */}
 
             <div className="grid justify-items-center">
